26d8ce9d2bf508c790f8007c241eb1be5f98d804
Bryan Martins <bryanmaraujo544@gmail.com>
2022-03-07 21:34:59 -0300

    setting prettier and eslint

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function App() {
+  return (
+    <Container>
+      <h1>App</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+`;
+
+export default global;
